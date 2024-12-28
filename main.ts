03cdaa64897caea268a9c4123efa491683b88a14
kergerf <francoiskerger@gmail.com>
2024-12-28 07:31:00 +0100

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,26 @@
+input.onButtonPressed(Button.A, function () {
+    wuKong.setAllMotor(vitesse, vitesse)
+    basic.pause(5000)
+    wuKong.setAllMotor(negVitesse, 0)
+    basic.pause(rotation_time)
+    wuKong.setAllMotor(vitesse, vitesse)
+    basic.pause(3000)
+    wuKong.setAllMotor(negVitesse, 0)
+    basic.pause(rotation_time)
+    wuKong.setAllMotor(vitesse, vitesse)
+    basic.pause(5000)
+    wuKong.setAllMotor(negVitesse, 0)
+    basic.pause(rotation_time)
+    wuKong.setAllMotor(vitesse, vitesse)
+    basic.pause(3000)
+    wuKong.stopAllMotor()
+})
+input.onButtonPressed(Button.AB, function () {
+    wuKong.stopAllMotor()
+})
+let rotation_time = 0
+let negVitesse = 0
+let vitesse = 0
+vitesse = 30
+negVitesse = -15
+rotation_time = 1200
